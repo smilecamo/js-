5f9cac45a423192aa280952ae03a0ff2b662e255
smilecamo <1191126635@qq.com>
2021-05-28 18:19:56 +0800

    2 files modified, 1 added

--- IIFE.js
+++ IIFE.js
@@ -1,11 +1,62 @@
 // 自执行函数
+// ----- 第一种 数组执行函数输出对应的值
+// let count = (function() {
+//   let count = 0;
+//   return function() {
+//     return ++count;
+//   };
+// })();
+// console.log(count());
+// console.log(count());
+// console.log(count());
 
-let count = (function() {
-  let count = 0;
-  return function() {
-    return ++count;
-  };
-})();
-console.log(count());
-console.log(count());
-console.log(count());
+// ------------------第一种
+// function foo() {
+//   var arr = [];
+//   for (var i = 0; i < 10; i++) {
+//     arr[i] = (function(n) {
+//       return function() {
+//         return n;
+//       };
+//     })(i);
+//   }
+//   return arr;
+// }
+// var fooArray = foo();
+// console.log(fooArray[0]());
+// console.log(fooArray[1]());
+// console.log(fooArray[2]());
+
+// ------------------第二种
+// function foo() {
+//   var arr = [];
+//   for (var i = 0; i < 10; i++) {
+//     (n => {
+//       arr[n] = function() {
+//         return n;
+//       };
+//     })(i);
+//   }
+//   return arr;
+// }
+// var fooArray = foo();
+// console.log(fooArray[0]());
+// console.log(fooArray[1]());
+// console.log(fooArray[2]());
+
+// --------------第三张
+
+// function foo() {
+//   var arr = [];
+//   for (let i = 0; i < 10; i++) {
+//     arr[i] = function() {
+//       return i;
+//     };
+//   }
+
+//   return arr;
+// }
+// var fooArray = foo();
+// console.log(fooArray[0]());
+// console.log(fooArray[1]());
+// console.log(fooArray[2]());
